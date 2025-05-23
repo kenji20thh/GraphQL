3f599d0859953451b45256c549e6a6a34583d2f1
kenji20thh <amineelayachi22@outlook.com>
2025-05-24 01:26:01 +0200

    logout-fixed

--- handleprofil.js
+++ handleprofil.js
@@ -1,2 +1,8 @@
 const token = localStorage.getItem('jwt')
 if (!token) window.location.replace('login.html')
+
+document.getElementById('logout-btn').addEventListener('click', () => {
+    localStorage.removeItem('jwt')
+    window.location.replace('login.html')
+})
+
