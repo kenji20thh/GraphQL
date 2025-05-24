ea1c7ba05ff4ead08f4111ad68730984ecb42ac9
kenji20thh <amineelayachi22@outlook.com>
2025-05-24 18:51:34 +0200

    changed-method-request

--- handleprofil.js
+++ handleprofil.js
@@ -6,3 +6,35 @@ document.getElementById('logout-btn').addEventListener('click', () => {
     window.location.replace('login.html')
 })
 
+const query = `{{
+  user {
+    id
+  }
+}}`
+
+const fetchUserData = async () => {
+    try {
+        const res = await fetch('https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ query })
+        })
+        if (!res.ok) throw new Error('failed at fetching user data')
+
+        const result = await res.json()
+        console.log(result)
+        const user = result.data.user[0]
+        if (!user) throw new Error('user not found')
+        document.getElementById('username').textContent = user.login
+        document.getElementById('email').textContent = user.email
+    } catch (error) {
+        console.error('error loading profile', error.message)
+    alert('OKKKK')
+    }
+
+}
+
+fetchUserData()
