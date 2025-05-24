eb42ce16b7aa4704e024abfec5269c377c256c48
kenji20thh <amineelayachi22@outlook.com>
2025-05-24 19:03:45 +0200

    Hasura

--- handleprofil.js
+++ handleprofil.js
@@ -6,35 +6,43 @@ document.getElementById('logout-btn').addEventListener('click', () => {
     window.location.replace('login.html')
 })
 
-const query = `{{
+const query = `
+{
   user {
     id
+    login
+    email
   }
-}}`
+}
+`
 
 const fetchUserData = async () => {
     try {
         const res = await fetch('https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql', {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
+                'Authorization': `Bearer ${token}`,
+                'X-Hasura-Role': 'student' // optional, but recommended
             },
             body: JSON.stringify({ query })
         })
+
         if (!res.ok) throw new Error('failed at fetching user data')
 
         const result = await res.json()
         console.log(result)
+
         const user = result.data.user[0]
         if (!user) throw new Error('user not found')
+
         document.getElementById('username').textContent = user.login
         document.getElementById('email').textContent = user.email
+
     } catch (error) {
         console.error('error loading profile', error.message)
-    alert('OKKKK')
+        alert('failed loading profile data')
     }
-
 }
 
 fetchUserData()
