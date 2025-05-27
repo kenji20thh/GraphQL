1451d131730eaef0ed2907f2ad0bca9829578c82
kenji20thh <amineelayachi22@outlook.com>
2025-05-27 17:46:25 +0200

    xp-amount-calculation

--- static/js/components/Skills.js
+++
@@ -1,9 +0,0 @@
-import { div } from "../bindjs/native.js";
-import { RadarChart } from "./RadarChart.js";
-
-export const Skills = (user) => {
-  return div("skills section").add(
-    div("title", "Skills"),
-    RadarChart(user.skills)
-  );
-};
--- static/js/components/Tooltip.js
+++
@@ -1,5 +0,0 @@
-import { div } from "../bindjs/native.js";
-
-export const Tooltip = (node, ...tip) => {
-  return div("tooltip-wrap").add(node, div("tooltip").add(...tip));
-};
